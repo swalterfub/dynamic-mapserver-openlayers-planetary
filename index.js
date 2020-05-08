77b1dd6321be0cdaf8dd2df1ad0f0522c10e5534
Sebastian Walter <swalter@zedat.fu-berlin.de>
2020-05-08 22:08:46 +0200

    mola base layer

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 import 'ol/ol.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import TileWMS from 'ol/source/TileWMS';
 
+var layers = [
+  new TileLayer({
+    source: new TileWMS({
+      url: 'https://maps.planet.fu-berlin.de/eqc-bin/wms?',
+      params: { LAYERS: 'MOLA-color-hs', VERSION: '1.1.1', TILED: true },
+      serverType: 'mapserver',
+      wrapX: true
+    }),
+  })
+];
 const map = new Map({
   target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM()
-    })
-  ],
+  layers: layers,
   view: new View({
     center: [0, 0],
-    zoom: 0
+    zoom: 0,
+    projection: 'EPSG:4326',
   })
 });
