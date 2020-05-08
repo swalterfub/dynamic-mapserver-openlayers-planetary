d4cdb1351b02103ae9d45598926870e3c32503b1
Sebastian Walter <swalter@zedat.fu-berlin.de>
2020-05-08 23:13:29 +0200

    iau2000:49910

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ import {register} from 'ol/proj/proj4';
 import {Projection} from 'ol/proj';
 import proj4 from 'proj4';
 
-proj4.defs('EPSG:49910', '+proj=eqc +lat_ts=0 +lat_0=0 +lon_0=0 +x_0=0 +y_0=0 +a=3396000 +b=3396000 +units=m +no_defs ');
+proj4.defs('IAU2000:49910', '+proj=eqc +lat_ts=0 +lat_0=0 +lon_0=0 +x_0=0 +y_0=0 +a=3396000 +b=3396000 +units=m +no_defs ');
 register(proj4);
 
 var projection = new Projection({
-  code: 'EPSG:49910',
+  code: 'IAU2000:49910',
   global: true,
   extent: [-10668848.652, -5215881.563, 10668848.652, 5215881.563]
 });
@@ -19,7 +19,7 @@ var projection = new Projection({
 var layers = [
   new TileLayer({
     source: new TileWMS({
-      url: 'https://maps.planet.fu-berlin.de/eqc-bin/wms?',
+      url: 'https://maps.planet.fu-berlin.de/eqc/',
       params: { LAYERS: 'MOLA-color-hs', VERSION: '1.1.1', TILED: true },
       serverType: 'mapserver',
       wrapX: true
