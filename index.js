94f3af7a103c292cd2692afa0eb0d02386fe3878
Sebastian Walter <swalter@zedat.fu-berlin.de>
2020-05-08 23:12:26 +0200

    epsg:49910

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import TileWMS from 'ol/source/TileWMS';
 import {defaults as defaultControls, ScaleLine} from 'ol/control';
+import {register} from 'ol/proj/proj4';
+import {Projection} from 'ol/proj';
+import proj4 from 'proj4';
+
+proj4.defs('EPSG:49910', '+proj=eqc +lat_ts=0 +lat_0=0 +lon_0=0 +x_0=0 +y_0=0 +a=3396000 +b=3396000 +units=m +no_defs ');
+register(proj4);
+
+var projection = new Projection({
+  code: 'EPSG:49910',
+  global: true,
+  extent: [-10668848.652, -5215881.563, 10668848.652, 5215881.563]
+});
 
 var layers = [
   new TileLayer({
@@ -26,6 +38,6 @@ const map = new Map({
   view: new View({
     center: [0, 0],
     zoom: 0,
-    projection: 'EPSG:4326',
+    projection: projection,
   })
 });
