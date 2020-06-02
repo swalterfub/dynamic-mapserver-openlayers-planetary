a2fbf495a94c1a21fbc3151fc95b3bbcce931079
Sebastian Walter <swalter@zedat.fu-berlin.de>
2020-06-02 17:34:37 +0200

    update code (gray mola, #map size, package, strip layer var)

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-import 'ol/ol.css';
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import TileWMS from 'ol/source/TileWMS';
+import "ol/ol.css";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import TileWMS from "ol/source/TileWMS";
 
-var layers = [
-  new TileLayer({
-    source: new TileWMS({
-      url: 'https://maps.planet.fu-berlin.de/eqc-bin/wms?',
-      params: { LAYERS: 'MOLA-color-hs', VERSION: '1.1.1', TILED: true },
-      serverType: 'mapserver',
-      wrapX: true
-    }),
-  })
-];
 const map = new Map({
-  target: 'map',
-  layers: layers,
+  target: "map",
+  layers: [
+    new TileLayer({
+      source: new TileWMS({
+        url: "https://maps.planet.fu-berlin.de/eqc-bin/wms?",
+        params: { LAYERS: "MOLA-gray-hs" }
+      })
+    })
+  ],
   view: new View({
     center: [0, 0],
     zoom: 0,
-    projection: 'EPSG:4326',
+    projection: "EPSG:4326"
   })
 });
