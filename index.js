22f6c047a7b628aa2fef1126899285ddc48f8189
Sebastian Walter <swalter@zedat.fu-berlin.de>
2020-06-02 22:51:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
 import 'ol/ol.css';
 import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import TileWMS from 'ol/source/TileWMS';
+import TileLayer from 'ol/layer/Tile';		
+import TileWMS from "ol/source/TileWMS";
 import {defaults as defaultControls, ScaleLine} from 'ol/control';
-
-var layers = [
-  new TileLayer({
-    source: new TileWMS({
-      url: 'https://maps.planet.fu-berlin.de/eqc-bin/wms?',
-      params: { LAYERS: 'MOLA-color-hs', VERSION: '1.1.1', TILED: true },
-      serverType: 'mapserver',
-      wrapX: true
-    }),
-  })
-];
-var scale = new ScaleLine({
-  units: 'metric'
-});
+ 
 const map = new Map({
   target: 'map',
+  layers: [
+    new TileLayer({
+      source: new TileWMS({
+        url: "https://maps.planet.fu-berlin.de/eqc-bin/wms?",
+        params: { LAYERS: "MOLA-gray-hs" }
+      })
+    })
+  ],
   controls: defaultControls().extend([
-    scale
+    new ScaleLine({
+      units: 'metric'
+    })
   ]),
-  layers: layers,
   view: new View({
     center: [0, 0],
     zoom: 0,
-    projection: 'EPSG:4326',
+    projection: 'EPSG:4326'
   })
 });
