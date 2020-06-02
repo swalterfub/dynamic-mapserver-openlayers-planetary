cbbba0f8edad0f8b288cdb271233b78fd89f7fae
Sebastian Walter <swalter@zedat.fu-berlin.de>
2020-06-02 16:17:47 +0200

    MapCache IAU2000

--- index.js
+++ index.js
@@ -8,19 +8,19 @@ import { Projection, getTransform, get } from "ol/proj";
 import { getDistance } from "ol/sphere";
 import proj4 from "proj4";
 
-proj4.defs("EPSG:49900", "+proj=longlat +a=3396000 +b=3396000 +no_defs ");
+proj4.defs("IAU2000:49900", "+proj=longlat +a=3396000 +b=3396000 +no_defs ");
 proj4.defs(
-  "EPSG:49910",
+  "IAU2000:49910",
   "+proj=eqc +lat_ts=0 +lat_0=0 +lon_0=0 +x_0=0 +y_0=0 +a=3396000 +b=3396000 +units=m +no_defs "
 );
 register(proj4);
 
 var projection = new Projection({
-  code: "EPSG:49910",
+  code: "IAU2000:49910",
   global: true,
   extent: [-10668848.652, -5215881.563, 10668848.652, 5215881.563],
   getPointResolution: function(resolution, point) {
-    var toEPSG49900 = getTransform(get("EPSG:49910"), get("EPSG:49900"));
+    var toEPSG49900 = getTransform(get("IAU2000:49910"), get("IAU2000:49900"));
     var vertices = [
       point[0] - resolution / 2,
       point[1],
@@ -37,13 +37,13 @@ const map = new Map({
   layers: [
     new TileLayer({
       source: new TileWMS({
-        url: "https://maps.planet.fu-berlin.de/eqc-bin/wms?",
+        url: "https://maps.planet.fu-berlin.de/eqc/?",
         params: { LAYERS: "MOLA-gray-hs" }
       })
     }),
     new TileLayer({
       source: new TileWMS({
-        url: "https://maps.planet.fu-berlin.de/eqc-bin/productid.py?",
+        url: "https://maps.planet.fu-berlin.de/eqc/?",
         params: { LAYERS: "hrsc4ihs", PRODUCTID: "h3286_0000.ihs.04" }
       })
     })
